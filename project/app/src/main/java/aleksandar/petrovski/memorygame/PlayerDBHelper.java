78845303d80d680157948ea70b16567fe2f1b465
Александар Петровски <97363871+husk-def@users.noreply.github.com>
2022-05-10 01:56:57 +0200

    Update PlayerDBHelper.java

--- project/app/src/main/java/aleksandar/petrovski/memorygame/PlayerDBHelper.java
+++ project/app/src/main/java/aleksandar/petrovski/memorygame/PlayerDBHelper.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
@@ -29,7 +30,7 @@ public class PlayerDBHelper extends SQLiteOpenHelper implements Serializable {
         /* select max(id) from player */
         Cursor cursor = db.query(
                 TABLE_NAME,
-                new String [] {"MAX(" + COLUMN_ID + ")"},
+                new String [] {COLUMN_ID},
                 null,
                 null,
                 null,
@@ -37,9 +38,10 @@ public class PlayerDBHelper extends SQLiteOpenHelper implements Serializable {
                 null
         );
         if (cursor.getCount() <= 0) {
-            ret = 0;
+            Log.i("moje", "getMaxId: empty database. return 0.");
         } else {
-            /* return that value + 1 */
+            /* return the last value (the highest one) + 1 */
+            cursor.moveToLast();
             ret = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID)) + 1;
         }
         cursor.close();
@@ -51,7 +53,7 @@ public class PlayerDBHelper extends SQLiteOpenHelper implements Serializable {
         super(context, name, factory, version);
     }
 
-    public void setid() {
+    public void setId() {
         idOffset = getMaxId();
     }
 
@@ -103,34 +105,38 @@ public class PlayerDBHelper extends SQLiteOpenHelper implements Serializable {
     }
 
     public User readUser(String userName) {
+        User retUser = null;
         SQLiteDatabase db = getReadableDatabase();
         //////////////////////////////////////////////////////////////////////////////
         /////////////////////////ako ista ne valja gledaj ovde////////////////////////
         //////////////////////////////////////////////////////////////////////////////
-        Cursor cursor = db.query(
-                TABLE_NAME,
-                null,
-                COLUMN_USERNAME + " =? ",
-                new String[] {userName},
-                null,
-                null,
-                null
-        );
+//        Cursor cursor = db.query(
+//                TABLE_NAME,
+//                null,
+//                COLUMN_USERNAME + " =? ",
+//                new String[] {userName},
+//                null,
+//                null,
+//                null
+//        );
+        String query = "SELECT * FROM " + TABLE_NAME + " where " + COLUMN_USERNAME + "=\"" + userName + "\"";
+        Cursor cursor = db.rawQuery(query, null);
         if (cursor.getCount() <= 0) {
-            return null;
-        }
-        cursor.moveToFirst();
-        User user = createUser(cursor);
-        for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
-            user.addScore(cursor.getColumnIndexOrThrow(COLUMN_POINTS));
+            Log.i("moje", "no such user: " + userName);
+        } else {
+            cursor.moveToFirst();
+            retUser = createUser(cursor);
+            for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+                retUser.addScore(cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_POINTS)));
+            }
         }
         db.close();
         cursor.close();
-        return user;
+        return retUser;
     }
 
     public ArrayList<User> readAllGames() {
-        ArrayList<User> games = new ArrayList<>();
+        ArrayList<User> games = null;
         SQLiteDatabase db = getReadableDatabase();
         Cursor cursor = db.query(
                 TABLE_NAME,
@@ -141,26 +147,35 @@ public class PlayerDBHelper extends SQLiteOpenHelper implements Serializable {
                 null,
                 null
         );
-        if(cursor.getCount() == 0) {
-            return null;
-        }
-        for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
-            games.add(createUser(cursor));
+        if (cursor.getCount() == 0) {
+            Log.i("moje", "readAllGames: no games.");
+        } else {
+            for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+                games = new ArrayList<>();
+                games.add(createUser(cursor));
+            }
         }
         db.close();
         return games;
     }
 
     public ArrayList<User> getDistinctUsers() {
-        ArrayList<User> distinct = new ArrayList<>();
+        ArrayList<User> distinct = null;
+        int cnt = 0;
         SQLiteDatabase db = getReadableDatabase();
-        String query = "SELECT DISTINCT * FROM " + TABLE_NAME;
+        String query = "SELECT DISTINCT Username, Email FROM " + TABLE_NAME;
         Cursor cursor = db.rawQuery(query, null);
 
-        for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
-            distinct.add(createUser(cursor));
+        if (cursor.getCount() == 0) {
+            Log.i("moje", "getDistinctUsers: no users.");
+        } else {
+            distinct = new ArrayList<>();
+            for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+                distinct.add(createUser(cursor));
+                ++cnt;
+            }
         }
-
+        Log.i("moje", "getDistinctUsers: " + Integer.toString(cnt) + " distinct users.");
         db.close();
         cursor.close();
         return distinct;
